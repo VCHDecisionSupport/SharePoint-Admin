f861212fb2a31e7ee5cd0821c649fe4a9718ce98
grahamcrowell <graham.crowell@gmail.com>
2016-12-01 12:21:37 -0800

    readme, started import commands

--- Bulk Import Export.ps1
+++ Bulk Import Export.ps1
@@ -1,8 +1,4 @@
 ﻿# generates batch files that contain bcp commands
-#   when executed batch files: 
-#     genereate xml format files, 
-#     export data into dat files, 
-#     and import dat files into target database
 #   create one batch command in each bat file for each view in database ($DatabaseName) on server ($ServerName)
 # $BcpFormatBatFile generates xml format files
 # $BcpExportBatFile exports sql data
@@ -18,7 +14,12 @@ $TargetFolder = "J:\shared\Export"
 $TargetFolder = "C:\Users\gcrowell\Documents\BulkExport"
 $BcpFormatBatFile = "SharePoint_bcp_make_format.bat"
 $BcpExportBatFile = "SharePoint_bcp_export.bat"
+$BcpExportBatFile = "SharePoint_bcp_import.bat"
 
+
+#########################################
+# setup folder that contains bat, xml, and dat files
+#########################################
 # check if folder exists
 if(-not(Test-Path -Path $TargetFolder))
 {
@@ -27,17 +28,17 @@ if(-not(Test-Path -Path $TargetFolder))
 }
 # set working directory to $TargetFolder
 Set-Location -Path $TargetFolder
-# check if bat file exists
-if(Test-Path -Path $BcpFormatBatFile)
-{
-    # delete bat if it exists
-    Remove-Item -Path $BcpFormatBatFile
-}
+# remove all bat batch files
+Get-ChildItem -Path $TargetFolder -Filter "*.bat" | ForEach-Object {Remove-Item -Path "$TargetFolder\$_"}
 # remove all xml format files
 Get-ChildItem -Path $TargetFolder -Filter "*.xml" | ForEach-Object {Remove-Item -Path "$TargetFolder\$_"}
 # remove all dat data files
 Get-ChildItem -Path $TargetFolder -Filter "*.dat" | ForEach-Object {Remove-Item -Path "$TargetFolder\$_"}
 
+
+#########################################
+# setup Smo database reader of source database 
+#########################################
 # load Smo library (Sql Management Objects) for access to database
 [void][Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
 # use Smo library to get a Server object and save it $SqlServer variable
@@ -46,6 +47,10 @@ $SqlServer = New-Object("Microsoft.SqlServer.Management.Smo.Server") $ServerName
 $SqlDatabases = $SqlServer.Databases
 # use $DatabaseName to select required database from $SqlDatabases list
 $SqlDatabase = $SqlDatabases[$DatabaseName]
+
+##########################################
+# generate batch files
+##########################################
 # use $Database object to get list of all views then loop over these views, generating bcp commands for each one and adding the commands to batch files
 foreach($SqlView in $SqlDatabase.Views | Where-Object {$_.IsSystemObject -eq $false -and $_.Schema -ne "sys"}) 
 {
@@ -55,6 +60,10 @@ foreach($SqlView in $SqlDatabase.Views | Where-Object {$_.IsSystemObject -eq $fa
     $FqSqlName = $SqlDatabase.Name+"."+$SqlView.Schema+"."+$SqlView.Name
     # print to screen
     Write-Host $FqSqlName
+
+    ##########################################
+    # bat to make xml format files
+    ##########################################
     $bcpFormatCommand = ("{0} format -x -c -T -f {1}.fmt -S {2}" -f $FqSqlName, $SqlTable.Name, $ServerName)
     # bcp Staging.dbo.Statement format nul -x -c -f C:\Users\user\Documents\BulkExport\export.xml -S localhost -T
     $bcpFormatCommand = ("{0} format nul -x -c -f {1}\{2}.xml -S {3} -T" -f $FqSqlName, $TargetFolder, $SqlView.Name, $ServerName)
@@ -63,10 +72,17 @@ foreach($SqlView in $SqlDatabase.Views | Where-Object {$_.IsSystemObject -eq $fa
     #Invoke-Command -ScriptBlock {"$bcpExePath $bcpCommand"}
     Out-File -FilePath $BcpFormatBatFile -Append -InputObject "bcp $bcpFormatCommand" -Encoding ascii
 
-
+    ##########################################
+    # bat to export data
+    ##########################################
     # export command
     # bcp FinDW.Dim.Stock out C:\Users\user\Documents\BulkExport\Stock.dat -x -c -f C:\Users\user\Documents\BulkExport\Stock.xml -S localhost -T
     $bcpExportCommand = ("{0} out {1}\{2}.dat -x -c -f {1}\{2}.xml -S {3} -T" -f $FqSqlName, $TargetFolder, $SqlView.Name, $ServerName)
     Out-File -FilePath $BcpExportBatFile -Append -InputObject "bcp $bcpExportCommand" -Encoding ascii
+    
+    ##########################################
+    # bat to import data
+    ##########################################
+    # bcp FinDW.Staging.FinancialStatement in "$flat_file" -f $xml_format_file -T -F 2 -e $bulk_import_log
 }
 Get-ChildItem
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # VchDsSharePoint
 General SharePoint code; usage analytics
+# `CREATE-TABLE-DSSPPROD_UsageAndHealth.sql`
+create destination tables (1 table per source view)
+
+# `Bulk Import Export.ps1`
+- when executed batch files: 
+ - genereate xml format files (execute on source database)
+ - export data into dat files  (execute on source database) 
+ - and import dat files into target database  (execute on destination database; destination table must already exist)
